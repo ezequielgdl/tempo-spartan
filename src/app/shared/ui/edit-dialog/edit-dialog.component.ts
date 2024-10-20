1cdd3bc8b81c447c87499afec50b78e277fb8f54
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-10-20 14:23:27 +0200

    reusable edit dialog

---
+++ src/app/shared/ui/edit-dialog/edit-dialog.component.ts
@@ -0,0 +1,87 @@
+
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { BrnDialogContentDirective, BrnDialogTriggerDirective } from '@spartan-ng/ui-dialog-brain';
+import {
+  HlmDialogComponent,
+  HlmDialogContentComponent,
+  HlmDialogDescriptionDirective,
+  HlmDialogFooterComponent,
+  HlmDialogHeaderComponent,
+  HlmDialogTitleDirective,
+} from '@spartan-ng/ui-dialog-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+
+@Component({
+  selector: 'app-edit-dialog',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    BrnDialogTriggerDirective,
+    BrnDialogContentDirective,
+
+    HlmDialogComponent,
+    HlmDialogContentComponent,
+    HlmDialogHeaderComponent,
+    HlmDialogFooterComponent,
+    HlmDialogTitleDirective,
+    HlmDialogDescriptionDirective,
+
+    HlmLabelDirective,
+    HlmInputDirective,
+    HlmButtonDirective,
+  ],
+  template: `
+    <hlm-dialog>
+      <button id="edit-profile" brnDialogTrigger hlmBtn>{{ buttonText }}</button>
+      <hlm-dialog-content class="sm:max-w-[425px]" *brnDialogContent="let ctx">
+        <hlm-dialog-header>
+          <h3 hlmDialogTitle>{{ title }}</h3>
+          <p hlmDialogDescription>{{ description }}</p>
+        </hlm-dialog-header>
+        <form [formGroup]="form" (ngSubmit)="onSave()">
+          <div class="py-4 grid gap-4">
+            @for (field of fields; track field.id) {
+              <div class="items-center grid grid-cols-4 gap-4">
+                <label hlmLabel [for]="field.id" class="text-right">{{ field.label }}</label>
+                <input hlmInput [id]="field.id" [formControlName]="field.id" class="col-span-3" />
+              </div>
+            }
+          </div>
+          <hlm-dialog-footer>
+            <button hlmBtn type="submit">{{ saveButtonText }}</button>
+          </hlm-dialog-footer>
+        </form>
+      </hlm-dialog-content>
+    </hlm-dialog>
+  `,
+})
+export class EditDialogComponent implements OnInit {
+  @Input() buttonText: string = 'Edit Profile';
+  @Input() title: string = 'Edit profile';
+  @Input() description: string = 'Make changes to your profile here. Click save when you\'re done.';
+  @Input() fields: Array<{id: string, label: string, value: string}> = [{id: 'name', label: 'Name', value: ''}];
+  @Input() saveButtonText: string = 'Save changes';
+  @Output() save = new EventEmitter<{[key: string]: string}>();
+
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    const formControls: {[key: string]: string} = {};
+    this.fields.forEach(field => {
+      formControls[field.id] = field.value;
+    });
+    this.form = this.fb.group(formControls);
+  }
+
+  onSave() {
+    if (this.form.valid) {
+      this.save.emit(this.form.value);
+    }
+  }
+}
+

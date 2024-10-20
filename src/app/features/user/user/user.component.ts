8bd3ae29607a62b7b8f6e275e5e8b87eebe8f587
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-10-20 14:12:48 +0200

    user component base

--- src/app/features/user/user/user.component.ts
+++ src/app/features/user/user/user.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { UserService } from '../services/user.service';
+import { UserInfo } from '../interface'
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [HlmInputDirective],
   template: `
-    <input hlmInput/>
+    <h1>Hola, {{ user()?.name }}</h1>
   `,
   styles: ``
 })
 export class UserComponent {
 
+user = signal<UserInfo | null>(null);
+
 constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUser().subscribe((user) => {
+      this.user.set(user);
+    });
+  }
 
 }

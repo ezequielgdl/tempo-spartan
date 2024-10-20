c4887282a161cfb44bf0b2ead5de9368c1506a2f
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-10-20 15:57:14 +0200

    navbar menu styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HlmButtonModule } from '@spartan-ng/ui-button-helm';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HlmButtonModule],
+  imports: [RouterOutlet, HlmButtonModule, NavbarComponent],
   template: `
+    <app-navbar />
     <router-outlet />
   `,
   styles: [],
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,16 +1,112 @@
-import { Component } from '@angular/core';
+
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '../../../core/auth/services/auth.service';
+import { provideIcons } from '@ng-icons/core';
+import {
+  lucideLayers,
+  lucideLogOut,
+  lucideUser,
+  lucideFile,
+  lucideBarChart
+} from '@ng-icons/lucide';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { BrnMenuTriggerDirective } from '@spartan-ng/ui-menu-brain';
+import {
+  HlmMenuComponent,
+  HlmMenuGroupComponent,
+  HlmMenuItemDirective,
+  HlmMenuItemIconDirective,
+  HlmMenuItemSubIndicatorComponent,
+  HlmMenuLabelComponent,
+  HlmMenuSeparatorComponent,
+  HlmMenuShortcutComponent,
+  HlmSubMenuComponent,
+} from '@spartan-ng/ui-menu-helm';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    BrnMenuTriggerDirective,
+
+    HlmMenuComponent,
+    HlmSubMenuComponent,
+    HlmMenuItemDirective,
+    HlmMenuItemSubIndicatorComponent,
+    HlmMenuLabelComponent,
+    HlmMenuShortcutComponent,
+    HlmMenuSeparatorComponent,
+    HlmMenuItemIconDirective,
+    HlmMenuGroupComponent,
+
+    HlmButtonDirective,
+    HlmIconComponent,
+  ],
+  providers: [
+    provideIcons({
+      lucideUser,
+      lucideLayers,
+      lucideLogOut,
+      lucideFile,
+      lucideBarChart
+    }),
+  ],
+  host: {
+    class: 'flex w-full items-center justify-between p-4'
+  },
   template: `
-    <p>
-      navbar works!
-    </p>
+  <div>Tempo</div>
+  <div>
+    <div class="flex w-full items-center justify-center">
+      <button hlmBtn variant="outline" align="end" [brnMenuTriggerFor]="menu">Menu</button>
+    </div>
+    <ng-template #menu>
+      <hlm-menu class="w-56">
+        <hlm-menu-label>Tempo</hlm-menu-label>
+        <hlm-menu-separator />
+        <hlm-menu-group>
+          <button hlmMenuItem routerLink="/user">
+            <hlm-icon name="lucideUser" hlmMenuIcon />
+            <span>Profile</span>
+            <hlm-menu-shortcut>⇧⌘P</hlm-menu-shortcut>
+          </button>
+
+          <button hlmMenuItem routerLink="/user">
+            <hlm-icon name="lucideLayers" hlmMenuIcon />
+            <span>Clients</span>
+            <hlm-menu-shortcut>⌘B</hlm-menu-shortcut>
+          </button>
+
+          <button hlmMenuItem routerLink="/user">
+            <hlm-icon name="lucideBarChart" hlmMenuIcon />
+            <span>Analytics</span>
+            <hlm-menu-shortcut>⌘S</hlm-menu-shortcut>
+          </button>
+
+          <button hlmMenuItem routerLink="/user">
+            <hlm-icon name="lucideFile" hlmMenuIcon />
+            <span>Invoices</span>
+            <hlm-menu-shortcut>⌘K</hlm-menu-shortcut>
+          </button>
+        </hlm-menu-group>
+
+        <hlm-menu-separator />
+
+        <button hlmMenuItem (click)="logout()">
+          <hlm-icon name="lucideLogOut" hlmMenuIcon />
+          <span>Logout</span>
+          <hlm-menu-shortcut>⇧⌘Q</hlm-menu-shortcut>
+        </button>
+      </hlm-menu>
+      </ng-template>
+    </div>
   `,
-  styles: ``
 })
 export class NavbarComponent {
+  constructor(private authService: AuthService) {}
 
+  logout() {
+    this.authService.logout();
+  }
 }

7a1d4f94222d4255a1d45123312f63b49bdd441d
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-10-23 18:33:58 +0200

    client component testing

--- src/app/features/clients/clients/clients.component.spec.ts
+++ src/app/features/clients/clients/clients.component.spec.ts
@@ -1,23 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClientsComponent } from './clients.component';
+import { ClientService } from '../services/clients.service';
+import { Client } from '../interface'
+import { of, throwError } from 'rxjs';
 
 describe('ClientsComponent', () => {
   let component: ClientsComponent;
   let fixture: ComponentFixture<ClientsComponent>;
+  let clientServiceSpy: jasmine.SpyObj<ClientService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ClientsComponent]
-    })
-    .compileComponents();
+    const spy = jasmine.createSpyObj('ClientService', ['getClients']);
 
+    await TestBed.configureTestingModule({
+      imports: [ClientsComponent],
+      providers: [
+        { provide: ClientService, useValue: spy }
+      ]
+    }).compileComponents();
+
+    clientServiceSpy = TestBed.inject(ClientService) as jasmine.SpyObj<ClientService>;
     fixture = TestBed.createComponent(ClientsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch clients on ngOnInit', () => {
+    const mockClients: Client[] = [
+      { id: '1', name: 'Client 1', address: 'Address 1', pricePerHour: 100 },
+      { id: '2', name: 'Client 2', address: 'Address 2', pricePerHour: 150 }
+    ];
+    clientServiceSpy.getClients.and.returnValue(of(mockClients));
+
+    component.ngOnInit();
+
+    expect(clientServiceSpy.getClients).toHaveBeenCalled();
+    expect(component.clients()).toEqual(mockClients);
+  });
+
+  it('should handle error when fetching clients', () => {
+    const error = new Error('Test error');
+    clientServiceSpy.getClients.and.returnValue(throwError(() => error));
+
+    component.ngOnInit();
+  });
+
+  it('should set the signal from the clients subscription', () => {
+    const mockClients: Client[] = [
+      { id: '1', name: 'Client 1', address: 'Address 1', pricePerHour: 100 },
+      { id: '2', name: 'Client 2', address: 'Address 2', pricePerHour: 150 }
+    ];
+    clientServiceSpy.getClients.and.returnValue(of(mockClients));
+
+    component.ngOnInit();
+
+    expect(component.clients()).toEqual(mockClients);
   });
 });
--- src/app/features/clients/components/clients-table/clients-table.component.spec.ts
+++ src/app/features/clients/components/clients-table/clients-table.component.spec.ts
@@ -20,4 +20,29 @@ describe('ClientsTableComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set the clients signal', () => {
+    component.clients = [
+      { id: '1', name: 'Client 1', address: 'Address 1', pricePerHour: 100 }
+    ];
+    expect(component.clients).toEqual([{ id: '1', name: 'Client 1', address: 'Address 1', pricePerHour: 100 }]);
+  });
+
+  it('should render the clients', () => {
+    component.clients = [
+      { id: '1', name: 'Client 1', address: 'Address 1', pricePerHour: 100 }
+    ];
+    fixture.detectChanges();
+    expect(fixture.nativeElement.textContent).toContain('Client 1');
+  });
+
+  it('should render several clients', () => {
+    component.clients = [
+      { id: '1', name: 'Client 1', address: 'Address 1', pricePerHour: 100 },
+      { id: '2', name: 'Client 2', address: 'Address 2', pricePerHour: 150 }
+    ];
+    fixture.detectChanges();
+    expect(fixture.nativeElement.textContent).toContain('Client 1');
+    expect(fixture.nativeElement.textContent).toContain('Client 2');
+  });
 });

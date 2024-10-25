9d43e5a09b79808238263d44950cb541fba035be
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-10-25 09:55:19 +0200

    edit user tests

--- src/app/features/user/components/edit-user/edit-user.component.spec.ts
+++ src/app/features/user/components/edit-user/edit-user.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { UserService } from '../../services/user.service';
 import { EditUserComponent } from './edit-user.component';
+import { UserInfo } from '../../interface';
+import { of } from 'rxjs';
 
-describe('EditUserComponent', () => {
+fdescribe('EditUserComponent', () => {
   let component: EditUserComponent;
   let fixture: ComponentFixture<EditUserComponent>;
+  let userService: UserService;
+  const mockUser: UserInfo = {
+    id: '1',
+    name: 'John Doe',
+    address: '123 Main St',
+    phone: '555-1234',
+    website: 'www.johndoe.com',
+    nif: '123456789',
+    iban: 'ES1234567890123456789012'
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditUserComponent]
+      imports: [EditUserComponent],
+      providers: [UserService]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(EditUserComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize fields', () => {
+    component.user = mockUser;
+    component.ngOnInit();
+    expect(component.fields.length).toBe(6);
+    expect(component.fields[0].value).toBe(mockUser.name);
   });
 });

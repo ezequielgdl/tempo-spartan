8d2d08f26df993978e561546059fe069b892b946
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-10-25 10:41:15 +0200

    delete client tests

--- src/app/features/clients/components/delete-client/delete-client.component.spec.ts
+++ src/app/features/clients/components/delete-client/delete-client.component.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteClientComponent } from './delete-client.component';
+import { ClientService } from '../../services/clients.service';
+import { of } from 'rxjs';
+import { Client } from '../../interface';
 
 describe('DeleteClientComponent', () => {
   let component: DeleteClientComponent;
   let fixture: ComponentFixture<DeleteClientComponent>;
+  let clientService: ClientService;
+  const mockClient: Client = {
+    id: '1',
+    name: 'John Doe',
+    address: '123 Main St',
+    pricePerHour: 100
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DeleteClientComponent]
+      imports: [DeleteClientComponent],
+      providers: [ClientService]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(DeleteClientComponent);
     component = fixture.componentInstance;
+    clientService = TestBed.inject(ClientService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call delete client', () => {
+    spyOn(clientService, 'deleteClient').and.returnValue(of(undefined));
+    component.onDelete(mockClient.id);
+    expect(clientService.deleteClient).toHaveBeenCalledWith(mockClient.id);
   });
 });

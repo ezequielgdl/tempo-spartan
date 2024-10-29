f497978ff0c98914923583769104415ef2b9eb7c
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-10-29 15:07:44 +0100

    new invoice button

--- src/app/features/invoices/invoices/invoices.component.ts
+++ src/app/features/invoices/invoices/invoices.component.ts
@@ -1,19 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { InvoicesServiceService } from '../services/invoices-service.service';
 import { InvoicesTableComponent } from '../components/invoices-table/invoices-table.component';
-import { ClientService } from '../../clients/services/clients.service';
 import { Invoice } from '../interface';
-import { Client } from '../../clients/interface';
 import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-invoices',
   standalone: true,
-  imports: [InvoicesTableComponent, HlmSpinnerComponent],
+  imports: [InvoicesTableComponent, HlmSpinnerComponent, HlmButtonDirective],
   host: {
     class: 'block max-w-5xl mx-auto w-full p-4 flex flex-col gap-6'
   },
   template: `
+    <button class="w-fit" hlmBtn (click)="createInvoice()">Create Invoice</button>
     @defer {
       <app-invoices-table [invoices]="invoices" />
     } @loading (minimum 300ms) {
@@ -26,11 +27,15 @@ import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
 export class InvoicesComponent {
   invoices = signal<Invoice[]>([]);
 
-  constructor(private invoicesService: InvoicesServiceService) {
+  constructor(private invoicesService: InvoicesServiceService, private router: Router) {
     this.invoicesService.invoices$.subscribe(invoices => {
       if (invoices) {
         this.invoices.set(invoices);
       }
     });
+  }
+
+  createInvoice() {
+    this.router.navigate(['/invoices/new']);
   }
 }

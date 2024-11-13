de1c3b2c9a3269a0e9584fa192de6fae85ed37f1
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-11-13 15:58:18 +0100

    redeploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+    supabaseUrl: 'https://bexfekwgojnzkyeeaxkf.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJleGZla3dnb2puemt5ZWVheGtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyODU3NTMsImV4cCI6MjA0NTg2MTc1M30.4oYwLVQAvAIhli_IqhaE9jKmD43ik0olj1bJADttUx8',
 };
